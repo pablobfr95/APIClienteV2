804f051b77707b8d1a915b23299f18299c1cd847
Pablo <pablo952014@gmail.com>
2021-01-21 15:03:56 -0300

    Inicio implementação Swagger

--- APICliente/APICliente.Serviços.API/Startup.cs
+++ APICliente/APICliente.Serviços.API/Startup.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using APICliente.Infra.CrossCutting.IoC;
 using APICliente.Infra.Data.Context;
@@ -12,6 +14,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.PlatformAbstractions;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
 
 namespace APICliente.Serviços.API
 {
@@ -35,9 +40,46 @@ namespace APICliente.Serviços.API
                    );
 
             InjetorDependencia.Registrar(services);
+
+            //services.AddSwaggerGen(options =>
+            //{
+            //    options.ExampleFilters();
+            //    options.EnableAnnotations();
+            //    options.SwaggerDoc(
+            //        "v1",
+            //        new OpenApiInfo
+            //        {
+            //            Version = "v1",
+            //            Title = "API Cliente",
+            //            Description = "Api de Clientes e Endereços"
+            //        });
+
+            //    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            //    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+            //    options.IncludeXmlComments(xmlPath);
+            //});
+
+            // services.AddSwaggerExamplesFromAssemblies(Assembly.GetEntryAssembly());
+
+            services.AddSwaggerGen(c => {
+
+                c.SwaggerDoc("v1",
+                    new OpenApiInfo
+                    {
+                        Title = "Comentários",
+                        Version = "v3.0",
+                        Description = "Exemplo de API REST criada com o ASP.NET Core 3.0 para cadastramento de comentários",
+                        Contact = new OpenApiContact
+                        {
+                            Name = "Renato Groffe",
+                            Url = new Uri("https://github.com/renatogroffe")
+                        }
+                    });
+            });
+
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+            // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -48,6 +90,11 @@ namespace APICliente.Serviços.API
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Indicadores Econômicos V1");
+            });
 
             app.UseEndpoints(endpoints =>
             {
